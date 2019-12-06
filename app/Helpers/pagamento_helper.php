8a082f3a1d43946502f97d7b97f226246f8229d2
Matheus Castro <matheuscastroweb@gmail.com>
2019-12-06 08:13:51 -0300

    Alteração do helper para limpar o código

--- app/Helpers/pagamento_helper.php
+++ app/Helpers/pagamento_helper.php
@@ -16,20 +16,20 @@ function getStatusCodePag(int $position = null): String
 {
     if (is_numeric($position)) {
         $types = array(
-            1 => '<span class="badge badge-info btn-block">Aguardando pagamento</span>',
-            2 => '<span class="badge badge-secondary btn-block">Em análise</span>',
-            3 => '<span class="badge badge-success btn-block">Paga</span>',
-            4 => '<span class="badge badge-success btn-block">Disponível</span>',
-            5 => '<span class="badge badge-warning btn-block">Em disputa</span>',
-            6 => '<span class="badge badge-info btn-block">Devolvida</span>',
-            7 => '<span class="badge badge-danger btn-block">Cancelada</span>',
-            8 => '<span class="badge badge-light btn-block">Debitado</span>',
-            9 => '<span class="badge badge-dark btn-block">Retenção temporária</span>',
+            1 => 'Aguardando pagamento',
+            2 => 'Em análise',
+            3 => 'Paga',
+            4 => 'Disponível',
+            5 => 'Em disputa',
+            6 => 'Devolvida',
+            7 => 'Cancelada',
+            8 => 'Debitado',
+            9 => 'Retenção temporária',
         );
 
-        return array_key_exists($position, $types) ? '<h5>' . $types[$position] . '</h5>' : '<span class="badge badge-danger">Não encontrado</span>';
+        return array_key_exists($position, $types) ? $types[$position] : 'Não encontrado';
     }
-    return '<span class="badge badge-danger">Não encontrado</span>';
+    return 'Não encontrado';
 }
 
 /**
