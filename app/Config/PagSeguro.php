7d27c4bf266ddd03eeb6257c72df048165c1ea93
Matheus Castro <matheuscastroweb@gmail.com>
2019-12-07 18:47:45 -0300

    Adição dos parâmetros $email e $token no Config/PagSeguro

--- app/Config/PagSeguro.php
+++ app/Config/PagSeguro.php
@@ -6,9 +6,22 @@ use CodeIgniter\Config\BaseConfig;
 
 /**
  * Classe responsável pelas URL da API do PagSeguro
+ * Ao configurar o CodeIgnter acessará essas configurações
  */
 class PagSeguro extends BaseConfig
 {
+    /**
+     * E-mail para configuração do PagSeguro
+     * @var String
+     */
+    public $email;
+
+    /**
+     * Token para configuração do PagSeguro
+     * @var String
+     */
+    public $token;
+
     /**
      * URL para gerar as sessões
      * Para alterar para modo e produção basta mudar para: https://ws.pagseguro.uol.com.br/v2/sessions
