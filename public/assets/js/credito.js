6073907aa8be97b888b99383e0c59432bcbbc579
Matheus Castro <matheuscastroweb@gmail.com>
2019-12-10 15:13:46 -0300

    Padronização de código credito.js

--- public/assets/js/credito.js
+++ public/assets/js/credito.js
@@ -1,21 +1,3 @@
-/**
- * Função responsável por validar e o número de parcelas e chamar a getInstallments
- */
-
-$("#parcelas").on('change', function (e) {
-	var parcelas = $(this).val();
-	if (parcelas != 0) {
-		getInstallments();
-		$('#btn_pagar').attr("disabled", false);
-		$('#btn_pagar').addClass("btn-success").removeClass("btn-danger");
-	} else {
-		$('#btn_pagar').attr("disabled", true);
-		$('#vparcela').val('');
-		$('#btn_pagar').addClass("btn-danger").removeClass("btn-success");
-	}
-});
-
-
 
 /**
  * Função da API para gerar o valor da parcela 
@@ -30,25 +12,77 @@ function getInstallments() {
 		brand: 'visa',
 		success: function (res) {
 			// Retorna as opções de parcelamento disponíveis
-			var valor_parcela = res.installments.visa[parc].installmentAmount;
+			var valorParcela = res.installments.visa[parc].installmentAmount;
 			var valor = res.installments.visa[parc].totalAmount;
 			/*
-			console.log('Valor parcela:' + valor_parcela);
+			console.log('Valor parcela:' + valorParcela);
 			console.log('Valor total:' + valor);
 			*/
-			$('#vparcela').val(parseFloat(valor_parcela));
+			$('#vparcela').val(parseFloat(valorParcela));
 			$('#valorTotal').val(parseFloat(valor));
 		},
 		error: function (response) {
-			alert('Erro getInstallments() in credito.js' + response.error)
+			alert('Erro getInstallments() in credito.js' + response.error);
 			// callback para chamadas que falharam.
-		},
-		complete: function (response) {
-			// Callback para todas chamadas.
 		}
 	});
 
 }
+
+
+/**
+ * Função responsável por validar e o número de parcelas e chamar a getInstallments
+ */
+
+$('#parcelas').on('change', function (e) {
+	var parcelas = $(this).val();
+	if (parcelas !== 0) {
+		getInstallments();
+		$('#btn_pagar').attr('disabled', false);
+		$('#btn_pagar').addClass('btn-success').removeClass('btn-danger');
+	} else {
+		$('#btn_pagar').attr('disabled', true);
+		$('#vparcela').val('');
+		$('#btn_pagar').addClass('btn-danger').removeClass('btn-success');
+	}
+});
+
+
+/**
+ * Função que realiza o pagamento de fato enviando os dados para o controller
+ */
+function pagarCartao() {
+
+	//Pega o hash de pagamento
+	$('#hash_pagamento').val(PagSeguroDirectPayment.getSenderHash());
+
+	$.ajax({
+		type: 'post',
+		url: 'gerarPagamento',
+		data: $('.form').serialize(),
+		dataType: 'json',
+		beforeSend: function () {
+			$('.msg').html('<div class="loading style-2"><div class="loading-wheel"></div></div');
+		}
+	}).done(function (res) {
+		console.log(res.error);
+		if (res.error === 0) {
+			$('#btn_pagar').val('Pagamento solicitado com sucesso');
+			$('.msg').html('Enviado com sucesso. Código da compra: ' + res.code.code);
+		} else {
+			$('.msg').html('Ocorreu um erro: ' + res.error + ' ' + res.message)
+		}
+	}).fail(function (res) {
+		$('.msg').html('Ocorreu um erro: ' + res.error + ' ' + res.message)
+
+	}).always(function (res) {
+		$('#parcelas').attr('disabled', true);
+		$('#btn_pagar').attr('disabled', true);
+	});
+
+}
+
+
 
 /**
  * Função responsável por verificar o token e fazer as atribuições necessárias para o cartão de crédito
@@ -60,7 +94,7 @@ function gerarToken(e) {
 	var numero = $('#ncartao').val();
 	var cvv = $('#cvv').val();
 	var validade = $('#validade').val();
-	validade = validade.split("/");
+	validade = validade.split('/');
 	var mes = validade[0];
 	var ano = validade[1];
 
@@ -86,41 +120,8 @@ function gerarToken(e) {
 		error: function (response) {
 			// Callback para chamadas que falharam.
 			alert('Erro ao gerar token de pagamento' + response.error);
-			console.log(response);
+			console.log(response.error);
 		}
 	});
 }
 
-/**
- * Função que realiza o pagamento de fato enviando os dados para o controller
- */
-function pagarCartao() {
-
-	//Pega o hash de pagamento
-	$('#hash_pagamento').val(PagSeguroDirectPayment.getSenderHash());
-
-	$.ajax({
-		type: 'post',
-		url: 'gerarPagamento',
-		data: $('.form').serialize(),
-		dataType: 'json',
-		beforeSend: function () {
-			$('.msg').html('<div class="loading style-2"><div class="loading-wheel"></div></div')
-		}
-	}).done(function (res) {
-		console.log(res);
-		if (res.error == 0) {
-			$('#btn_pagar').val('Pagamento solicitado com sucesso');
-			$('.msg').html('Enviado com sucesso. Código da compra: ' + res.code.code);
-		} else {
-			$('.msg').html('Ocorreu um erro: ' + res.error + ' ' + res.message)
-		}
-	}).fail(function (res) {
-		$('.msg').html('Ocorreu um erro: ' + res.error + ' ' + res.message)
-
-	}).always(function (res) {
-		$('#parcelas').attr("disabled", true);
-		$('#btn_pagar').attr("disabled", true);
-	});
-
-}
